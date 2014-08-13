89be4441ea8257aa94d6a1a8c1a0afacc1a20603
Daniel A. Torquato <daniel.antela.torquato@gmail.com>
2014-08-12 22:06:37 -0400

    Vrp Final

--- src/MyMethod/vrp.hpp
+++ src/MyMethod/vrp.hpp
@@ -19,16 +19,36 @@ double cust_route(route X) {
  double aux=0;
  for( int i=0; i<X.size()-1; i++) 
   aux+=std::abs(X[i]-X[i+1]);
- aux+=std::abs(*X.end()-*X.begin());
+ aux+=std::abs(X[X.size()-1]-X[0]);
 
  return aux;
 }
 
 double cust( train X) {
  double aux=0;
- for( int i=0; i<X.size()-1; i++) 
-  aux+=cust_route(X[i]); 
 
+ for( int i=0; i<X.size(); i++) 
+  aux+=cust_route(X[i]); 
  return aux;
+
+/*
+ for( int i=0; i<X.size(); i++) 
+  aux+=pow(cust_route(X[i]),2); 
+
+ return pow(aux,0.5);
+*/
+/*
+ double d[2]={0,cust_route(X[0])};
+ for( int i=1; i<X.size(); i++) {
+  d[0]=cust_route(X[i]);
+
+//  std::cout << d[0] << std::endl;
+  if(d[0]>d[1]) {
+   d[1]=d[0];
+  }
+ }
+// std::cout << d[1] << std::endl;
+ aux=pow(pow(aux,2)+pow(d[1],2),0.5);
+*/
 }
 
