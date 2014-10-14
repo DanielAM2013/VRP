cbc941f8f190fc5f6a8f02249cc5ab022d959088
Daniel A. Torquato <daniel.antela.torquato@gmail.com>
2014-10-14 13:31:23 -0400

    Modelamento

---
+++ src/CPP/Graph/lib/basic.hpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <queue>
+#include <ctime>
+#include <algorithm>
+
+#include <complex>
+
+typedef unsigned int num;
+typedef unsigned int vertex;
+typedef double weight;
+typedef std::complex<double> point;
+
+/*
+class edge
+{
+ private:
+  point weight;
+ public:  
+  edge( double re=0, double im=0): weight(re, im) {}
+ 
+  point getw( void) {
+   return weight;
+  }
+  void nullw( void) {
+   weight=point(0,0);
+  }
+
+}
+
+class vertex
+{
+ private:
+  double info;
+ public:
+  vertex( double I): info(I) {}
+
+  double geti(void) {
+   return info;
+  }
+
+}
+
+class arcs;
+class ladj;
+class madj;
+*/
---
+++ src/CPP/Graph/src/digraph.cpp
@@ -0,0 +1,13 @@
+#include "basic.hpp"
+#include "arcs.hpp"
+#include "madj.hpp"
+#include "ladj.hpp"
+
+class digraph: public madj, public ladj, public arcs 
+{
+ private:
+ protected:
+ public:
+
+
+}
