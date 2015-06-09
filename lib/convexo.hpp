ca7508ea35b27d0c9b13a492180c40224347ed53
Daniel A. Torquato <daniel.antela.torquato@gmail.com>
2015-06-09 19:27:44 -0400

    9 7 2015

--- lib/convexo.hpp
+++ lib/convexo.hpp
@@ -3,13 +3,21 @@
 
 #include <route.hpp>
 
+//! this function return cross product to vectors A and B
+//! which is the area of retangle gerate for deslocation A along of B signalized:
+//! positive if A is counterclockwise B, and negative otherwise
+//! cust: O(1)
 double sign( point A, point B) {
  return (A.real()*B.imag()-A.imag()*B.real());
 }
 
+
+//! this function return true if X is inside of V concex
+//! cust: O(size(V))
 bool In ( route V, point X) {
 
  uint n=V.size();
+
  for ( uint k=0; k<n-1; k++)
   if ( sign(V[k+1]-V[k], X-V[k]) > 0)
    return false;
@@ -20,6 +28,7 @@ bool In ( route V, point X) {
  return true;
 }
 
+//! 
 uint positive( route V, point X) {
 
  uint n=V.size();
@@ -71,11 +80,15 @@ route bordo_convexo( route X ) {
  route fecho;
  route AUX(X);
 
+ //! this get two first points
  fecho.push_back(AUX[0]);
  fecho.push_back(AUX[1]);
 
+ //! this erase two first points
  AUX.erase(AUX.begin(), AUX.begin()+2);
 
+
+ //! add point if that is in out of convex
  for( uint k=2; k<AUX.size(); k++)
   if( sign(AUX[k]-fecho[0],fecho[1]-fecho[0]) >0) {
    fecho.push_back(AUX[k]);
@@ -83,11 +96,19 @@ route bordo_convexo( route X ) {
    break;
   }
 
+ //! add points if that is out of convex
  for( uint k=2; k<AUX.size(); k++) {
+   //! check if point k is inside of convex
    if( In(fecho, AUX[k])) continue;
+   //! this get the index v of last point from left and 
+   //! firt point from right to edge
    uint v=positive( fecho, AUX[k]), w=negative( fecho, AUX[k]);
+
+   //! erase point between points v and w
    apagar(fecho, v, w);
-   if( w>v)
+
+   //! add out point at v+1 position 
+   if( w > v)
     fecho.insert(fecho.begin()+v+1, AUX[k]);
    else
     fecho.insert(fecho.begin(), AUX[k]);
